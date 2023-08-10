a42754894d8fdcc24383ede0388ee957902792d7
Moshood1426 <moshood.yemi10@gmail.com>
2023-08-10 05:40:02 -0400

    All users page initiated in the frontend and single user component initiated

---
+++ web/pages/users/index.tsx
@@ -0,0 +1,14 @@
+//DO THE USERS PAGE HERE AND THE MODAL TO ADD NEW USER ALSO HERE
+
+interface AllUsersProps {
+  id: number;
+  name: string;
+  email: string;
+  avatar: string;
+}
+
+const AllUsers: React.FC<AllUsersProps> = () => {
+  return <div>
+    
+  </div>;
+};
